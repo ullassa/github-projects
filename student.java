a511074cb7136f05a6f88c746f9b3d5d8614fa90
ULLAS S A <ullas200410@gmail.com>
2025-03-13 19:17:56 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class student {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+        
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.println("Enter marks for Subject " + i + " (out of 100): ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        double average = (double) totalMarks / numSubjects;
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", average);
+
+        String grade;
+        if (average >= 90) {
+            grade = "A+";
+        } else if (average >= 80) {
+            grade = "A";
+        } else if (average >= 70) {
+            grade = "B";
+        } else if (average >= 60) {
+            grade = "C";
+        } else if (average >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
